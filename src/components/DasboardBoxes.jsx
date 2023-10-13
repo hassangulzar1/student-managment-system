12bd45dca709c6e8cec8fd7ae0207403393e55f6
hassangulzar1 <hassangulzar121@gmail.com>
2023-10-13 23:34:58 +0500

    logo designing left

--- src/components/DasboardBoxes.jsx
+++ src/components/DasboardBoxes.jsx
@@ -1,19 +1,14 @@
 import React from "react";
 import classes from "./DashboardBoxes.module.css";
-import SchoolOutlinedIcon from "@mui/icons-material/SchoolOutlined";
-const DasboardBoxes = (props) => {
+
+const DasboardBoxes = ({ className, titleColor, title, count, icon }) => {
+  const styles = className;
   return (
-    <div className={`${classes.box} ${classes.studentBox}`}>
+    <div className={`${classes.box} ${classes[styles]}`}>
       <div style={{ marginLeft: "1rem", marginTop: ".9rem" }}>
-        <SchoolOutlinedIcon sx={{ fontSize: "3rem" }} />
-        <p
-          className={classes.student}
-          style={{ color: props.color, marginTop: ".2rem" }}
-        >
-          {/* {props.title} */}
-          Students
-        </p>
-        <h5>243</h5>
+        <icon sx={{ fontSize: "3rem" }} />
+        <p style={{ color: titleColor, marginTop: ".2rem" }}>{title}</p>
+        <h5>{count}</h5>
       </div>
     </div>
   );
--- src/pages/1-Dashboard/Dashboard.jsx
+++ src/pages/1-Dashboard/Dashboard.jsx
@@ -1,20 +1,29 @@
 import React, { useContext } from "react";
 import authContext from "../../Context/auth-context";
 import DashboardBoxes from "../../components/DasboardBoxes";
+import SchoolOutlinedIcon from "@mui/icons-material/SchoolOutlined";
+import { Done } from "@mui/icons-material";
 
 const Dashboard = () => {
   const ctx = useContext(authContext);
+
   return (
     <div className="d-flex justify-content-center">
-      <DashboardBoxes className="studentBox" />
-      <DashboardBoxes />
+      <DashboardBoxes
+        className="studentBox"
+        titleColor="black"
+        title="Students"
+        count={ctx.studentsData.length}
+        SchoolOutlinedIcon={Done}
+      />
+      <DashboardBoxes
+        className="coursesBox"
+        titleColor="black"
+        title="Courses"
+        count={ctx.courcesData.length}
+      />
       <DashboardBoxes />
     </div>
-
-    // <div style={{ margin: "auto" }}>
-    //   <h1>Total Students: {ctx.studentsData.length}</h1>
-    //   <h1>Cources: {ctx.courcesData.length}</h1>
-    // </div>
   );
 };
 
