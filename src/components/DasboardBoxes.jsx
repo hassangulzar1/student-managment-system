7689c7e545dc6b1f5e9824fc8ffee16de4c7d104
hassangulzar1 <hassangulzar121@gmail.com>
2023-10-14 22:35:16 +0500

    card ready

--- src/components/DasboardBoxes.jsx
+++ src/components/DasboardBoxes.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import classes from "./DashboardBoxes.module.css";
 
-const DasboardBoxes = ({ className, titleColor, title, count, icon }) => {
+const DasboardBoxes = ({ className, titleColor, title, count, Icon }) => {
   const styles = className;
   return (
     <div className={`${classes.box} ${classes[styles]}`}>
-      <div style={{ marginLeft: "1rem", marginTop: ".9rem" }}>
-        <icon sx={{ fontSize: "3rem" }} />
+      <div style={{ marginLeft: "1rem", marginTop: "1.5rem" }}>
+        {Icon}
         <p style={{ color: titleColor, marginTop: ".2rem" }}>{title}</p>
         <h5>{count}</h5>
       </div>
--- src/pages/1-Dashboard/Dashboard.jsx
+++ src/pages/1-Dashboard/Dashboard.jsx
@@ -2,11 +2,9 @@ import React, { useContext } from "react";
 import authContext from "../../Context/auth-context";
 import DashboardBoxes from "../../components/DasboardBoxes";
 import SchoolOutlinedIcon from "@mui/icons-material/SchoolOutlined";
-import { Done } from "@mui/icons-material";
-
+import BookmarkBorderOutlinedIcon from "@mui/icons-material/BookmarkBorderOutlined";
 const Dashboard = () => {
   const ctx = useContext(authContext);
-
   return (
     <div className="d-flex justify-content-center">
       <DashboardBoxes
@@ -14,13 +12,14 @@ const Dashboard = () => {
         titleColor="black"
         title="Students"
         count={ctx.studentsData.length}
-        SchoolOutlinedIcon={Done}
+        Icon={<SchoolOutlinedIcon sx={{ fontSize: "3rem" }} />}
       />
       <DashboardBoxes
         className="coursesBox"
         titleColor="black"
         title="Courses"
         count={ctx.courcesData.length}
+        Icon={<BookmarkBorderOutlinedIcon sx={{ fontSize: "3rem" }} />}
       />
       <DashboardBoxes />
     </div>
