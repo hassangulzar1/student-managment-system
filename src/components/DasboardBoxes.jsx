69aa1ce8fb2a51e974ef88c56350d2e84bf18e53
hassangulzar1 <hassangulzar121@gmail.com>
2023-10-20 23:16:46 +0500

    click box to navigate done

--- src/components/DasboardBoxes.jsx
+++ src/components/DasboardBoxes.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import classes from "./DashboardBoxes.module.css";
 import CountUp from "react-countup";
-
+import { useNavigate } from "react-router-dom";
 const DasboardBoxes = ({
   className,
   titleColor,
@@ -10,9 +10,24 @@ const DasboardBoxes = ({
   Icon,
   spinnerColor,
 }) => {
+  const navigate = useNavigate();
   const styles = className;
+  //! navigation
+  const navigateHandler = (navigateTo) => {
+    if (navigateTo == "Students") {
+      navigate("/students");
+    } else if (navigateTo == "Courses") {
+      navigate("/courses");
+    } else {
+      navigate("/attendence");
+    }
+  };
+
   return (
-    <div className={`${classes.box} ${classes[styles]}`}>
+    <div
+      className={`${classes.box} ${classes[styles]}`}
+      onClick={() => navigateHandler(title)}
+    >
       <div style={{ marginLeft: "1rem", marginTop: "1.5rem" }}>
         {Icon}
         <p style={{ color: titleColor, marginTop: ".2rem" }}>{title}</p>
